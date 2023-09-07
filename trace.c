c411a02d9a7c22e0e7af5b0c2a6d71446e7f8bab
liuh-80 <liuh@microsoft.com>
2023-09-07 14:34:50 +0800

    Ignore trace log when debug disabled.

--- trace.c
+++ trace.c
@@ -6,9 +6,16 @@
 
 #include "trace.h"
 
+/* Tacacs control flag */
+extern int tacacs_ctrl;
+
 /* Output trace log. */
 void trace(const char *format, ...)
 {
+    if (tacacs_ctrl & PAM_TAC_DEBUG == 0) {
+        return;
+    }
+
     // convert log to a string because va args resoursive issue:
     // http://www.c-faq.com/varargs/handoff.html
     char logBuffer[MAX_LINE_SIZE];
